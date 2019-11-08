9aa7d2ba5859cb7986d67106fd98b65a01e3503b
nhulannguyen <vupro1512@gmail.com>
2019-11-08 02:52:21 +0100

    Add developing

--- src/Hellogit.java
+++ src/Hellogit.java
@@ -1,5 +1,24 @@
 public class Hellogit {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("developing");
+        System.out.println("developing....");
+        System.out.println("developing....");
+        System.out.println("developing....");
+        System.out.println("developing....");
+        System.out.println("developing....");
+        System.out.println("developing....");
+        System.out.println("developing....");
+        System.out.println("developing....");
+        System.out.println("developing....");
+        System.out.println("developing....");
+        System.out.println("developing....");
+        System.out.println("developing....");
+        System.out.println("developing....");
+        System.out.println("developing....");
+        System.out.println("developing....");
+        System.out.println("developing....");
+        System.out.println("developing....");
+        System.out.println("developing....");
     }
 }
